2759f8c259536133696c9070291b026c5ed9d018
HAIL <s000715@naver.com>
2020-06-06 07:03:16 +0900

    version 0.1

---
+++ google-Translator.py
@@ -0,0 +1,98 @@
+#version 0.1 (2020-06-06)
+#made by Seoung-su 'HAIL' NOH
+
+import sys
+from PyQt5.QtWidgets import *
+from googletrans import Translator
+
+
+class MyApp(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.translator = Translator()
+        self.initUI()
+
+    def initUI(self):
+        #KortoEn
+        self.KorEn = QWidget()
+        self.lbl1 = QLabel()
+        self.lbl1.setText('번역할 텍스트를 입력하세요')
+        self.before1 = QTextEdit()
+        self.lbl2 = QLabel()
+        self.lbl2.setText('번역 결과입니다')
+        self.after1 = QTextEdit()
+
+        vbox1 = QVBoxLayout()
+        vbox1.addWidget(self.lbl1)
+        vbox1.addWidget(self.before1)
+        vbox1.addWidget(self.lbl2)
+        vbox1.addWidget(self.after1)
+        self.KorEn.setLayout(vbox1)
+
+        self.before1.textChanged.connect(self.translate_kor)
+
+        #EntoKor
+        self.EnKor = QWidget()
+        self.lbl3 = QLabel()
+        self.lbl3.setText('번역할 텍스트를 입력하세요')
+        self.before2 = QTextEdit()
+        self.lbl4 = QLabel()
+        self.lbl4.setText('번역 결과입니다')
+        self.after2 = QTextEdit()
+
+        vbox2 = QVBoxLayout()
+        vbox2.addWidget(self.lbl3)
+        vbox2.addWidget(self.before2)
+        vbox2.addWidget(self.lbl4)
+        vbox2.addWidget(self.after2)
+        self.EnKor.setLayout(vbox2)
+
+        self.before2.textChanged.connect(self.translate_en)
+
+        #Tab
+        tabs = QTabWidget()
+        tabs.addTab(self.KorEn, '한글을 영어로')
+        tabs.addTab(self.EnKor, '영어를 한글로')
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(tabs)
+        self.setLayout(vbox)
+
+        #WindowSetting
+        self.setWindowTitle('Google Translator')
+        self.setGeometry(200, 200, 400, 300)
+        self.show()
+
+    def translate_kor(self):
+        text_kor = self.before1.toPlainText()
+        if len(text_kor) != 0: #영한번역에서 텍스트를 입력했다가 지우면 강제종료되는 경우가 있어 예외처리했다.
+            text_en = self.translator.translate(text_kor).text
+            self.after1.setText(text_en)
+
+    def translate_en(self):
+        text_en = self.before2.toPlainText()
+        if len(text_en) != 0: #텍스트를 입력했다가 지우면 강제종료되는 경우가 있어 예외처리했다.
+            text_kor = self.translator.translate(text_en, src="en", dest="ko").text
+            #src="번역 전 언어", dest="번역 후 언어" 로 설정한다.
+            self.after2.setText(text_kor)
+
+    def closeEvent(self, event):
+        endMessage = QMessageBox()
+        endMessage.setWindowTitle("종료하기")
+        endMessage.setText("번역기를 종료하시겠습니까?")
+        endMessage.setDetailedText("써주셔서 감사합니다 :D")
+
+
+        reply = endMessage.question(self, "", endMessage.text(),
+                                    QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+        if reply == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
